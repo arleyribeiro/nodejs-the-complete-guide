5283b40b6c6aad6f8bb9e62906a0f7cb08f3af3e
arleyribeiro <arley.sribeiro@gmail.com>
2020-03-31 08:26:04 -0300

    32 - Redirect requesting

--- section-3-understanding-the-basics/server/app.js
+++ section-3-understanding-the-basics/server/app.js
@@ -1,7 +1,16 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log(req);
+    const url = req.url;
+    if (url === '/') {
+        res.write('<html><head><title>Enter a message</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text"/><button type="submit">send</button></form></body></html>');
+        return res.end();
+    }
+    res.setHeader('Content-type', 'text/html');
+    res.write('<html><head><title>My First Page node</title></head>');
+    res.write('<body><h1>Hello from my Node.Js Server!</h1></body></html>');
+    res.end();
 });
 
 server.listen(3000);
