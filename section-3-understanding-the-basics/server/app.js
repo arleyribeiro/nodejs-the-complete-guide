e1efb9ea850a63f7a3260f423671164bd7a2e14d
arleyribeiro <arley.sribeiro@gmail.com>
2020-04-01 05:23:48 -0300

    36 - Blocking and No-Blocking code

--- section-3-understanding-the-basics/server/app.js
+++ section-3-understanding-the-basics/server/app.js
@@ -17,14 +17,15 @@ const server = http.createServer((req, res) => {
             body.push(chunk);
         });
 
-        req.on('end', () => {
+        return req.on('end', () => {
             const parsedBody = Buffer.concat(body).toString();
             const message = parsedBody.split('=')[1];
-            fs.writeFileSync('message.text', message);
+            fs.writeFile('message.text', message, (err) => {
+                res.statusCode = 302;
+                res.setHeader('Location', '/');
+                return res.end();     
+            });
         })
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();     
     }
     res.setHeader('Content-type', 'text/html');
     res.write('<html><head><title>My First Page node</title></head>');
