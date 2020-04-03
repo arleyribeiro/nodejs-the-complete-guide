a22ae1df7e3be6ad1eebe2ca2068933d494692cf
arleyribeiro <arley.sribeiro@gmail.com>
2020-04-03 05:37:30 -0300

    section-4 - Handling different routes

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
-const http = require('http');
-const routes = require('./routes');
+const expresss = require('express');
 
-const server = http.createServer(routes);
+const app = expresss();
 
-server.listen(3000);
+app.use('/add-product', (req, res, next) => {
+    console.log('In the middleware');
+    res.send('<h1>The add product page</h1>');
+});
+
+app.use('/', (req, res, next) => {
+    console.log('In another middleware');
+    res.send('<h1>Hello from express!</h1>');
+});
+
+app.listen(3000);
