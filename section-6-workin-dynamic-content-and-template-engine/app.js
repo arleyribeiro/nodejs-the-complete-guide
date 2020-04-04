270c518ef53f0581d692dbeea87673078c356223
arleyribeiro <arley.sribeiro@gmail.com>
2020-04-04 09:37:05 -0300

    Finish converting files in ejs

--- section-6-workin-dynamic-content-and-template-engine/app.js
+++ section-6-workin-dynamic-content-and-template-engine/app.js
@@ -8,7 +8,7 @@ const shopRoutes = require('./routes/shop');
 
 const app = expresss();
 
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(expresss.static(path.join(__dirname, 'public')));
