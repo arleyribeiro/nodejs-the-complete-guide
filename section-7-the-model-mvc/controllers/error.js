16c648a984302606bac311b12b8ad294140a1efb
arleyribeiro <arley.sribeiro@gmail.com>
2020-04-05 07:18:28 -0300

    correction for path undefined

--- section-7-the-model-mvc/controllers/error.js
+++ section-7-the-model-mvc/controllers/error.js
@@ -1,3 +1,3 @@
 exports.get404 = (req, res, next) => {
-    res.status(404).render('404', {docTitle: 'Page Not Found'});
+    res.status(404).render('404', {docTitle: 'Page Not Found', path: '/error'});
 };
