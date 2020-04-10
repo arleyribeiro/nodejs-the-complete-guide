6aef65c4b05a491341d0874908ad7ee38b8f137e
arleyribeiro <arley.sribeiro@gmail.com>
2020-04-10 06:57:03 -0300

    152 - syncing js definitions to the database

--- section-11-understanding-sequelize/app.js
+++ section-11-understanding-sequelize/app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const db = require('./util/database');
+const sequelize = require('./util/database');
 
 const app = express();
 
@@ -14,14 +14,6 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-db.execute('SELECT * FROM products')
-    .then(result => {
-        console.log(result[0], result[1])
-    })
-    .catch(err => {
-        console.log("fetch database: ", err);
-    });
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -30,4 +22,10 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3000);
+sequelize.sync()
+    .then(result => {
+        // console.log(result)
+        app.listen(3000);
+    })
+    .catch((err) => console.log(err));
+
--- section-11-understanding-sequelize/models/product.js
+++ section-11-understanding-sequelize/models/product.js
@@ -26,3 +26,5 @@ const Product = sequelize.define('product', {
     allowNull: false
   }
 });
+
+module.exports = Product;
