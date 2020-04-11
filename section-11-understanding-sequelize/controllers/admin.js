c3c65607de1e3fb57da06645607a4f92965b4393
arleyribeiro <arley.sribeiro@gmail.com>
2020-04-11 06:11:00 -0300

    164 - fetching related products

--- section-11-understanding-sequelize/controllers/admin.js
+++ section-11-understanding-sequelize/controllers/admin.js
@@ -13,6 +13,7 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
+  /* this method was created by sequelize, relationship, belongsTo or hasMany */
   req.user.createProduct({
     title: title,
     description: description,
@@ -34,31 +35,34 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId = req.params.productId;
-  Product.findByPk(prodId)
-    .then(product => {
-      if (!product) {
-        return res.redirect('/');
-      }
-      res.render('admin/edit-product', {
-        pageTitle: 'Edit Product',
-        path: '/admin/edit-product',
-        editing: editMode,
-        product: product
-      });
-    })
-    .catch(err => console.log(err));
+  req.user
+    .getProducts({ where: { id: prodId } }) //this method was created by sequelize, relationship, belongsTo or hasMany
+      .then(products => {
+        const product = products[0];
+        if (!product) {
+          return res.redirect('/');
+        }
+        res.render('admin/edit-product', {
+          pageTitle: 'Edit Product',
+          path: '/admin/edit-product',
+          editing: editMode,
+          product: product
+        });
+      })
+      .catch(err => console.log(err));
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll()
-    .then(products => {
-      res.render('admin/products', {
-        prods: products,
-        pageTitle: 'Admin Products',
-        path: '/admin/products'
-      });
-    })
-    .catch(err => console.log(err));
+  req.user
+    .getProducts()
+      .then(products => {
+        res.render('admin/products', {
+          prods: products,
+          pageTitle: 'Admin Products',
+          path: '/admin/products'
+        });
+      })
+      .catch(err => console.log(err));
 };
 
 
