022538ec7a1d0efa2a7c4a65e3df8c763e578243
arleyribeiro <arley.sribeiro@gmail.com>
2020-04-14 04:57:46 -0300

    195 - working on cart items & orders

--- section-12-working-with-nosql-using-mongo-db/models/user.js
+++ section-12-working-with-nosql-using-mongo-db/models/user.js
@@ -3,10 +3,11 @@ const mongodb = require('mongodb')
 const getDb = require('../util/database').getDb;
 
 class User {
-    constructor(username, email, id) {
+    constructor(username, email, id, cart) {
         this.name = username;
         this.email = email;
         this._id = id ? new mongodb.ObjectID(id) : null;
+        this.cart = cart;
     }
 
     save() {
@@ -24,6 +25,21 @@ class User {
                     return user;
                 })
                 .catch(err => console.log(err));
+    }
+
+    addToCart (product) {
+        const db = getDb();
+        const cartProduct = this.cart.items.findIndex(cp => {
+            return cp._id === product._id;
+        });
+
+        const updatedCart = { items: [{ ...product, quantity: 1 }] };
+        return db.collection('users')
+            .updateOne({ id: this._id }, { $set: {cart: updatedCart }})
+            .then(user => {
+                return user;
+            })
+            .catch(err => console.log(err));
     }
 
     static findById (id) {
