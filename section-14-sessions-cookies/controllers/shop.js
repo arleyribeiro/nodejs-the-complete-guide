8ad21ed4761b4ba8cbd992d563eecc9e40c57b0e
arleyribeiro <arley.sribeiro@gmail.com>
2020-04-17 08:11:10 -0300

    232 - Optional: creating the login form

---
+++ section-14-sessions-cookies/controllers/shop.js
@@ -0,0 +1,131 @@
+const Product = require('../models/product');
+const Order = require('../models/order');
+
+exports.getProducts = (req, res, next) => {
+  Product.find()
+    .then(products => {
+      res.render('shop/product-list', {
+        prods: products,
+        pageTitle: 'All Products',
+        path: '/products'
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+exports.getProductById = (req, res, next) => {
+  const productId = req.params.productId;
+  Product.findById(productId)
+  .then((product) => {
+    res.render('shop/product-detail', {
+      product: product,
+      pageTitle: 'Details Product',
+      path: '/products'
+    });
+  })
+  .catch(err => {
+    console.log(err)
+  });
+};
+
+exports.getIndex = (req, res, next) => {
+  Product.find()
+    .then((products) => {
+      res.render('shop/index', {
+        prods: products,
+        pageTitle: 'Shop',
+        path: '/'
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+exports.getCart = (req, res, next) => {
+  req.user
+    .populate('cart.items.productId')
+    .execPopulate()
+      .then(user => {
+        const products = user.cart.items;
+        console.log("PRODUCTS: ", products)
+        res.render('shop/cart', {
+          path: '/cart',
+          pageTitle: 'Your Cart',
+          prods: products
+        });
+      })
+      .catch(err => console.log(err));
+};
+
+exports.postCart = (req, res, next) => {
+  const productId = req.body.productId;
+  Product.findById(productId)
+    .then(product => {
+      return req.user.addToCart(product);
+    })
+    .then(result => {
+      console.log(result);
+      res.redirect('/cart');
+    })
+    .catch(err => console.log(err));
+};
+
+exports.postDeleteCart = (req, res, next) => {
+  const prodId = req.body.productId;
+  console.log("prodId: ", prodId)
+  req.user.removeFromCart(prodId)
+    .then(result => {
+      res.redirect('/cart');
+    })
+    .catch(err => console.log(err));
+};
+
+exports.postOrder = (req, res, next) => {
+  req.user
+  .populate('cart.items.productId')
+  .execPopulate()
+    .then(user => {
+      const products = user.cart.items.map(p => {
+        return { quantity: p.quantity, product: { ...p.productId._doc } }
+      });
+      console.log('products order: ', products)
+      const order = new Order({
+        user: {
+          name: req.user.name,
+          userId: req.user
+        },
+        products: products
+      });
+      return order.save();
+    })
+    .then(() => {
+      return req.user.clearCart();
+    })
+    .then(result => {
+      res.redirect('/orders');
+    })
+    .catch(err => console.log(err));
+};
+
+exports. getOrders = (req, res, next) => {
+  Order
+    .find({ "user.userId": req.user._id })
+    .then(orders => {
+      res.render('shop/orders', {
+        path: '/orders',
+        pageTitle: 'Your Orders',
+        orders: orders
+      });
+    })
+    .catch(err => console.log(err));
+};
+
+exports.getCheckout = (req, res, next) => {
+  res.render('shop/checkout', {
+    path: '/checkout',
+    pageTitle: 'Checkout'
+  });
+};
---
+++ section-14-sessions-cookies/routes/auth.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+const express = require('express');
+
+const authController = require('../controllers/auth');
+
+const router = express.Router();
+
+router.get('/login', authController.getLogin);
+
+module.exports = router;
