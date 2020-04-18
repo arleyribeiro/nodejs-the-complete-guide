0610b53d647dd343bf12a5567e624d267acbc8e5
arleyribeiro <arley.sribeiro@gmail.com>
2020-04-18 06:58:49 -0300

    238 - Initializing the session middleware

--- section-14-sessions-cookies/app.js
+++ section-14-sessions-cookies/app.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose')
+const session = require('express-session')
 
 const errorController = require('./controllers/error');
 const User = require('./models/user')
@@ -17,6 +18,12 @@ const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({ 
+        secret: 'my secret but that is not secret', 
+        resave: false, 
+        saveUninitialized: false
+    })
+);
 
 app.use((req, res, next) => {
     User.findById('5e981b3639f1061f258aa9ae')
--- section-14-sessions-cookies/controllers/auth.js
+++ section-14-sessions-cookies/controllers/auth.js
@@ -1,14 +1,13 @@
 exports. getLogin = (req, res, next) => {
-    const isLoggedIn = req.get('Cookie').split(';')[0].trim().split('=')[1];
     res.render('auth/login', {
         path: '/login',
         pageTitle: 'Login',
-        isAuthenticated: isLoggedIn
+        isAuthenticated: false
     });
 };
 
 exports. postLogin = (req, res, next) => {
-    res.setHeader('Set-Cookie', 'loggedIn=true');
+    res.setHeader('Set-Cookie', 'loggedIn=true; HttpOnly');
     res.redirect('/');
 };
 
