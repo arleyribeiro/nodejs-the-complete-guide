8443b88ca5ba0bb07c9be69344b0419f3337f690
arleyribeiro <arley.sribeiro@gmail.com>
2020-04-21 06:06:03 -0300

    262 - fixing the order button

--- section-15-adding-authentication/controllers/shop.js
+++ section-15-adding-authentication/controllers/shop.js
@@ -97,7 +97,7 @@ exports.postOrder = (req, res, next) => {
       console.log('products order: ', products)
       const order = new Order({
         user: {
-          name: req.user.name,
+          email: req.user.email,
           userId: req.user
         },
         products: products
