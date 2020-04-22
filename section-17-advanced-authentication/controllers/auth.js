fc1e9b479bc93099efe7db6dbb6567e35298cc2d
arleyribeiro <arley.sribeiro@gmail.com>
2020-04-22 05:44:16 -0300

    276 - resetting passwords

---
+++ section-17-advanced-authentication/controllers/auth.js
@@ -0,0 +1,107 @@
+const bcrypt = require('bcryptjs');
+const User = require('../models/user');
+
+exports. getLogin = (req, res, next) => {
+    let message = req.flash('error');
+    message = message.length > 0 ? message[0] : null;
+    res.render('auth/login', {
+        path: '/login',
+        pageTitle: 'Login',
+        errorMessage: message
+    });
+};
+
+exports. postLogin = (req, res, next) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    User.findOne({ email: email })
+    .then(user => {
+        if (!user) {
+            req.flash('error', 'Invalid email or password.');
+            return res.redirect('/login');
+        }
+        bcrypt.compare(password, user.password)
+            .then(doMatch => {
+                if (doMatch) {
+                    req.session.isLoggedIn = true;
+                    req.session.user = user;
+                    return req.session.save(err => {
+                        console.log(err);
+                        res.redirect('/');
+                    });
+                } else {
+                    req.flash('error', 'Invalid email or password.');
+                    res.redirect('/login');
+                }
+            })
+            .catch(err => { 
+                console.log(err);
+                res.redirect('/login');
+            });
+    })
+    .catch(err => console.log(err));
+};
+
+exports. postLogout = (req, res, next) => {
+    req.session.destroy((err) => {
+        console.log(err);
+        res.redirect('/');
+    });
+}
+
+exports. getSignup = (req, res, next) => {
+    let message = req.flash('error');
+    message = message.length > 0 ? message[0] : null;
+    res.render('auth/signup', {
+        path: '/signup',
+        pageTitle: 'Signup',
+        errorMessage: message
+    });
+};
+
+exports. postSignup = (req, res, next) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    const confirmPassword = req.confirmPassword;
+
+    User.findOne({ email: email })
+    .then(userDoc => {
+        if (userDoc) {
+            req.flash('error', 'E-mail exists already pick a different one.');
+            return res.redirect('/');
+        }
+        return bcrypt.hash(password, 12)
+            .then(hashPassword => {
+                const user = new User({
+                    email: email,
+                    password: hashPassword,
+                    cart: { items: [] }
+                });
+                return user.save();
+            })
+            .then(user => {
+                if (user) {
+                    res.redirect('/login')
+                }
+            });
+    })
+    .catch(err => console.log(err));
+};
+
+exports.getReset = (req, res, next) => {
+    let message = req.flash('error');
+    message = message.length > 0 ? message[0] : null;
+    res.render('auth/reset', {
+        path: '/reset',
+        pageTitle: 'Reset password',
+        errorMessage: message        
+    })
+};
+
+exports.postReset = (req, res, next) => {
+    res.render('auth/reset', {
+        path: '/reset',
+        pageTitle: 'Reset password'        
+    })
+};
+
---
+++ section-17-advanced-authentication/models/user.js
@@ -0,0 +1,65 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  password: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  cart: {
+      items: [ 
+            {
+              productId: { 
+                  type: Schema.Types.ObjectId,
+                  ref: 'Product',
+                  required: true 
+                }, 
+              quantity: { 
+                  type: Number, 
+                  required: true 
+                } 
+            } 
+        ]
+  }
+});
+
+userSchema.methods.addToCart = function (product) {
+    let quantity = 1;
+    let updatedCartItems = [];
+    let cartProductIndex;
+    if (this.cart && this.cart.items) {
+        cartProductIndex = this.cart.items.findIndex(cp => {
+            return cp.productId.toString() === product._id.toString();
+        });
+        updatedCartItems = [ ...this.cart.items];
+    }
+    if (cartProductIndex >= 0) {
+        quantity += this.cart.items[cartProductIndex].quantity;
+        updatedCartItems[cartProductIndex].quantity = quantity;
+    } else {
+        updatedCartItems.push({ productId: product._id, quantity: quantity })
+    }
+    const updatedCart = { items: updatedCartItems };
+    this.cart = updatedCart;
+    return this.save();
+}
+
+userSchema.methods.removeFromCart = function (productId) {
+    const updatedItemsCart = this.cart.items.filter(product => {
+        return product.productId.toString() !== productId.toString();
+    });
+    this.cart.items = updatedItemsCart;
+    return this.save();
+}
+
+userSchema.methods.clearCart = function () {
+    this.cart = { items: [] };
+    return this.save();
+}
+
+module.exports = mongoose.model('User', userSchema);
