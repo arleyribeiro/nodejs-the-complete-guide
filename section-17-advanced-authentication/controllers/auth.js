c447a1951e33f15837a13f41b877dd2d3e97f9e4
arleyribeiro <arley.sribeiro@gmail.com>
2020-04-22 07:01:09 -0300

    Fixing crypto.randomBytes is not a function

--- section-17-advanced-authentication/controllers/auth.js
+++ section-17-advanced-authentication/controllers/auth.js
@@ -2,6 +2,7 @@ const bcrypt = require('bcryptjs');
 const User = require('../models/user');
 const crypto = require('crypto');
 const nodemailer = require('nodemailer');
+const secureRandom = require('secure-random');
 
 const transporter = nodemailer.createTransport({
     service: 'gmail',
@@ -124,39 +125,38 @@ exports.getReset = (req, res, next) => {
 };
 
 exports.postReset = (req, res, next) => {
-    crypto.randomBytes(32, (err, buffer) => {
-        if (err) {
-            console.log(err);
-            return res.redirect('/reset');
-        }
+    const buffer = secureRandom(32, {type: 'Buffer'});
+    if (buffer) {
         const token = buffer.toString('hex');
-    });
-    User.findOne({ email: req.body.email  })
-    .then((user) => {
-        if (!user) {
-            req.flash(error, 'No account with that email found.');
-            return res.redirect('/reset');
-        }
-        user.resetToken = token;
-        user.resetTokenExpiration = Date.now() + 3600000;
-        return user.save();
-    })
-    .then(result => {
-        res.redirect('/');
-        return transporter.sendMail({
-            to: req.body.email,
-            from: 'shop@node-complete.com',
-            subject: 'Reset!',
-            html: `<p> You required a password reset</p>
-                    <p> Click this <a href="http://localhost:3000/reset/${token}">link</a> to set a new password.</p>`
-        }, (err, info) => {
-            if (err) {
-                console.log(err);                            
-            } else {
-                console.log(info);
-            }
-        });
-    })
-    .catch(err => console.log(err));
+        User.findOne({ email: req.body.email  })
+            .then((user) => {
+                if (!user) {
+                    req.flash('error', 'No account with that email found.');
+                    return res.redirect('/reset');
+                }
+                user.resetToken = token;
+                user.resetTokenExpiration = Date.now() + 3600000;
+                return user.save()
+                    .then(result => {
+                        res.redirect('/');                    
+                        return transporter.sendMail({
+                            to: req.body.email,
+                            from: 'shop@node-complete.com',
+                            subject: 'Reset!',
+                            html: `<p> You required a password reset</p>
+                                    <p> Click this <a href="http://localhost:3000/reset/${token}">link</a> to set a new password.</p>`
+                        }, (err, info) => {
+                            if (err) {
+                                console.log(err);                            
+                            } else {
+                                console.log(info);
+                            }
+                        });
+                    })
+            })            
+            .catch(err => console.log(err));
+    } else {
+        return res.redirect('/reset');
+    }
 };
 
