924dd9ab957536536474212bd2f0b09aa2e18188
arleyribeiro <arley.sribeiro@gmail.com>
2020-04-23 06:41:51 -0300

    294 - checking for field equality

--- section-18-understanding-validation/routes/auth.js
+++ section-18-understanding-validation/routes/auth.js
@@ -26,9 +26,17 @@ router.post(
                 return true;
             }),
         body('password',
-            'Please enter a password with only numbers and text and at least 5 characters.')
+            'Please enter a password with only numbers and text and at least 5 characters.'
+            )
             .isLength({ min: 2})
-            .isAlphanumeric()
+            .isAlphanumeric(),
+        body('confirmPassword')
+            .custom((value, { req }) => {
+                if (value !== req.body.password) {
+                    throw new Error('Passwords have to match!')
+                }
+                return true;
+            })
     ],
     authController.postSignup);
 
