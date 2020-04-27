355dc44de1c1e1864f76a9ce80d5a557da98684c
arleyribeiro <arley.sribeiro@gmail.com>
2020-04-27 05:40:45 -0300

    Section 20

---
+++ section-20-file-upload-download/controllers/shop.js
@@ -0,0 +1,157 @@
+const Product = require('../models/product');
+const Order = require('../models/order');
+
+exports.getProducts = (req, res, next) => {
+  Product.find()
+    .then(products => {
+      res.render('shop/product-list', {
+        prods: products,
+        pageTitle: 'All Products',
+        path: '/products'
+      });
+    })
+    .catch(err => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
+    });
+};
+
+exports.getProductById = (req, res, next) => {
+  const productId = req.params.productId;
+  Product.findById(productId)
+  .then((product) => {
+    res.render('shop/product-detail', {
+      product: product,
+      pageTitle: 'Details Product',
+      path: '/products'
+    });
+  })
+  .catch(err => {
+    const error = new Error(err);
+    error.httpStatusCode = 500;
+    return next(error);
+  });
+};
+
+exports.getIndex = (req, res, next) => {
+  Product.find()
+    .then((products) => {
+      res.render('shop/index', {
+        prods: products,
+        pageTitle: 'Shop',
+        path: '/'
+      });
+    })
+    .catch(err => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
+    });
+};
+
+exports.getCart = (req, res, next) => {
+  req.user
+    .populate('cart.items.productId')
+    .execPopulate()
+      .then(user => {
+        const products = user.cart.items;
+        console.log("PRODUCTS: ", products)
+        res.render('shop/cart', {
+          path: '/cart',
+          pageTitle: 'Your Cart',
+          prods: products
+        });
+      })
+      .catch(err => {
+        const error = new Error(err);
+        error.httpStatusCode = 500;
+        return next(error);
+      });
+};
+
+exports.postCart = (req, res, next) => {
+  const productId = req.body.productId;
+  Product.findById(productId)
+    .then(product => {
+      return req.user.addToCart(product);
+    })
+    .then(result => {
+      console.log(result);
+      res.redirect('/cart');
+    })
+    .catch(err => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
+    });
+};
+
+exports.postDeleteCart = (req, res, next) => {
+  const prodId = req.body.productId;
+  console.log("prodId: ", prodId)
+  req.user.removeFromCart(prodId)
+    .then(result => {
+      res.redirect('/cart');
+    })
+    .catch(err => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
+    });
+};
+
+exports.postOrder = (req, res, next) => {
+  req.user
+  .populate('cart.items.productId')
+  .execPopulate()
+    .then(user => {
+      const products = user.cart.items.map(p => {
+        return { quantity: p.quantity, product: { ...p.productId._doc } }
+      });
+      console.log('products order: ', products)
+      const order = new Order({
+        user: {
+          email: req.user.email,
+          userId: req.user
+        },
+        products: products
+      });
+      return order.save();
+    })
+    .then(() => {
+      return req.user.clearCart();
+    })
+    .then(result => {
+      res.redirect('/orders');
+    })
+    .catch(err => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
+    });
+};
+
+exports. getOrders = (req, res, next) => {
+  Order
+    .find({ "user.userId": req.user._id })
+    .then(orders => {
+      res.render('shop/orders', {
+        path: '/orders',
+        pageTitle: 'Your Orders',
+        orders: orders
+      });
+    })
+    .catch(err => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
+    });
+};
+
+exports.getCheckout = (req, res, next) => {
+  res.render('shop/checkout', {
+    path: '/checkout',
+    pageTitle: 'Checkout'
+  });
+};
