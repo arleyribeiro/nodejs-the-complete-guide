1cfbc046826326589eaaa4823e2f81e92462d631
arleyribeiro <arley.sribeiro@gmail.com>
2020-05-01 07:05:32 -0300

    350 - Manipulating the DOM

--- section-22-understanding-async-requests/public/js/admin.js
+++ section-22-understanding-async-requests/public/js/admin.js
@@ -2,7 +2,7 @@ const deleteProduct = (btn) => {
     console.log('clicked')
     const prodId = btn.parentNode.querySelector('[name=productId').value;
     const csrf = btn.parentNode.querySelector('[name=_csrf').value;
-
+		const productElement = btn.closest('article');
     fetch(`/admin/product/${prodId}`, {
 				method: 'DELETE',
 				headers: {
@@ -10,7 +10,10 @@ const deleteProduct = (btn) => {
 				}
 		})
 		.then(result => {
-			console.log(result)
+			return result.json();
+		})
+		.then(data => {
+			productElement.parentNode.removeClild(productElement);
 		})
 		.catch(result => {
 			console.log(result)
