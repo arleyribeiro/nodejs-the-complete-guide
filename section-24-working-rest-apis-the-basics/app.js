ae11836bda43d1763508820bddacd76cfb06bf79
arleyribeiro <arley.sribeiro@gmail.com>
2020-05-01 15:27:53 -0300

    364 - rest apis, clients & cors errors

--- section-24-working-rest-apis-the-basics/app.js
+++ section-24-working-rest-apis-the-basics/app.js
@@ -8,6 +8,14 @@ const app = express();
 // Setup app for application/json
 app.use(bodyParser.json({ extended: true }));
 
+// Set CORS errors
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
 // Set routes
 app.use('/feed', feedsRoutes);
 
