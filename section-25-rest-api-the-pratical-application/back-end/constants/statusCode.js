05a0ed51a149bc5bf370cf2dba536f7604ffd6de
arleyribeiro <arley.sribeiro@gmail.com>
2020-05-02 08:54:46 -0300

    Constants of api

---
+++ section-25-rest-api-the-pratical-application/back-end/constants/statusCode.js
@@ -0,0 +1,46 @@
+const ok = 200;
+const created = 201;
+const accepted = 202;
+
+const badRequest = 400;
+const unauthorized = 401;
+const forbidden = 403;
+const unprecessableEntity = 422;
+
+const internalServerError = 500;
+
+class StatusCode {
+  static get OK() {
+    return ok;
+  }
+
+  static get CREATED() {
+    return created;
+  }
+
+  static get ACCEPTED() {
+    return accepted;
+  }
+
+  static get BAD_REQUEST() {
+    return badRequest;
+  }
+
+  static get UNAUTHORIZED() {
+    return unauthorized;
+  }
+
+  static get FORBIDDEN() {
+    return forbidden;
+  }
+
+  static get UNPRECESSABLE_ENTITY() {
+    return unprecessableEntity;
+  }
+
+  static get INTERNAL_SERVER_ERROR() {
+    return internalServerError;
+  }
+}
+
+module.exports = StatusCode;
