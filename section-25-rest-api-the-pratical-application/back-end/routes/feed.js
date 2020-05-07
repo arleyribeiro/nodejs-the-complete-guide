b7a69698239a727b0fc006db01a332bcb3a0ddde
arleyribeiro <arley.sribeiro@gmail.com>
2020-05-07 07:30:06 -0300

    390. Using & Validating the token

--- section-25-rest-api-the-pratical-application/back-end/routes/feed.js
+++ section-25-rest-api-the-pratical-application/back-end/routes/feed.js
@@ -1,10 +1,11 @@
 const express = require('express');
 
-const feedController = require('../controllers/feed')
+const feedController = require('../controllers/feed');
+const isAuth = require('../middleware/is-auth');
 
 const router = express.Router();
 
-router.get('/posts', feedController.getPosts);
+router.get('/posts', isAuth, feedController.getPosts);
 
 router.post('/post', feedController.PostValidator(), feedController.createPost);
 
