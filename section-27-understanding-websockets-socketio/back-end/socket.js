6c6dfc0c9581cb5ad59d3fea6bc8c99747954c9b
arleyribeiro <arley.sribeiro@gmail.com>
2020-05-11 06:51:17 -0300

    fixing socket.js

--- section-27-understanding-websockets-socketio/back-end/socket.js
+++ section-27-understanding-websockets-socketio/back-end/socket.js
@@ -2,7 +2,7 @@ let io;
 
 module.exports = {
   init: httpServer => {
-    io = require('socket.io').init(httpServer);
+    io = require('socket.io')(httpServer);
     return io;
   },
   getIO: () => {
