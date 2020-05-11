2ebb7f46d1bfe2752a169e8cb1003b115c29aeff
arleyribeiro <arley.sribeiro@gmail.com>
2020-05-11 06:25:34 -0300

    410. synchronizing post additions

--- section-27-understanding-websockets-socketio/front-end/src/pages/Feed/Feed.js
+++ section-27-understanding-websockets-socketio/front-end/src/pages/Feed/Feed.js
@@ -40,7 +40,12 @@ class Feed extends Component {
       .catch(this.catchError);
 
     this.loadPosts();
-    openSocket('http://localhost:8080');
+    const socket = openSocket('http://localhost:8080');
+    socket.on('posts', data => {
+      if (data.action === 'create') {
+        this.addPost(data.post);
+      }
+    });
   }
 
   addPost = post => {
@@ -183,8 +188,6 @@ class Feed extends Component {
               p => p._id === prevState.editPost._id
             );
             updatedPosts[postIndex] = post;
-          } else if (prevState.posts.length < 2) {
-            updatedPosts = prevState.posts.concat(post);
           }
           return {
             posts: updatedPosts,
