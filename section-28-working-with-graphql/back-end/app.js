ca5ecf0f1951e0b2950dec61c5617837972b345e
arleyribeiro <arley.sribeiro@gmail.com>
2020-05-13 20:38:48 -0300

    423. handling errors

--- section-28-working-with-graphql/back-end/app.js
+++ section-28-working-with-graphql/back-end/app.js
@@ -53,7 +53,16 @@ app.use((req, res, next) => {
 app.use('/graphql', graphqlHttp({
   schema: graphqlSchema,
   rootValue: graphqlResolver,
-  graphiql: true
+  graphiql: true,
+  formatError(err) {
+    if (!err.originalError) {
+      return err;
+    }
+    const data = err.originalError.data;
+    const message = err.message || 'An error ocurred!';
+    const code = err.originalError.code || 500;
+    return { message: message, status: code, data: data };
+  }
 }));
 
 app.use((error, req, res, next) => {
--- section-28-working-with-graphql/back-end/graphql/resolvers.js
+++ section-28-working-with-graphql/back-end/graphql/resolvers.js
@@ -15,6 +15,8 @@ module.exports = {
     }
     if (errors.length > 0) {
       const error = new Error('Invalid input');
+      error.data = errors;
+      error.code = 402;
       throw error;
     }
     const existngUser = await User.findOne({ email: userInput.email });
