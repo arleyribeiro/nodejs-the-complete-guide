220a8fe493bb7f43af91fc38a9cef2beb812c111
arleyribeiro <arley.sribeiro@gmail.com>
2020-05-18 07:20:34 -0300

    450. setting secure response headers with helmet

--- section-29-deploying-our-app/app.js
+++ section-29-deploying-our-app/app.js
@@ -7,6 +7,7 @@ const MongoDBStore = require('connect-mongodb-session')(session);
 const csrf = require('csurf');
 const flash = require('connect-flash');
 const multer = require('multer');
+const helmet = require('helmet');
 
 const errorController = require('./controllers/error');
 const User = require('./models/user')
@@ -29,6 +30,8 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const authRoutes = require('./routes/auth');
+
+app.use(helmet());
 
 const filesStorage = multer.diskStorage({
     destination: (req, file, cb) => {
