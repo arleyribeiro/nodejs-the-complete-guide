ab36960dd328218708d56f2b91e07f52af4db7e9
arleyribeiro <arley.sribeiro@gmail.com>
2020-05-20 06:09:48 -0300

    467. organizing multiple tests

--- section-30-testing-nodejs-applications/test/auth-middleware.js
+++ section-30-testing-nodejs-applications/test/auth-middleware.js
@@ -1,11 +1,22 @@
 const authMiddleware = require('../middleware/is-auth');
 const expect = require('chai').expect;
 
-it('should throw an error if no authorization header is present', function() {
-  const req = {
-    get: function(headerName) {
-      return null;
-    }
-  };
-  expect(authMiddleware.bind(this, req, {}, () => {})).to.throw('Not authenticaded.');
+describe('Auth Middleware', function() {
+  it('should throw an error if no authorization header is present', function() {
+    const req = {
+      get: function(headerName) {
+        return null;
+      }
+    };
+    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw('Not authenticaded.');
+  });
+  
+  it('should throw an error if no authorization header is only one string', function() {
+    const req = {
+      get: function(headerName) {
+        return 'unittest';
+      }
+    };
+    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
+  });
 });
