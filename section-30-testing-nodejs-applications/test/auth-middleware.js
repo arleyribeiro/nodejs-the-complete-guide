2794ee5768323d970ee3c96cdc2be2b8a79ed808
arleyribeiro <arley.sribeiro@gmail.com>
2020-05-20 06:30:18 -0300

    469. using stubs

--- section-30-testing-nodejs-applications/test/auth-middleware.js
+++ section-30-testing-nodejs-applications/test/auth-middleware.js
@@ -1,5 +1,8 @@
-const authMiddleware = require('../middleware/is-auth');
 const expect = require('chai').expect;
+const jwt = require('jsonwebtoken');
+const sinon = require('sinon');
+
+const authMiddleware = require('../middleware/is-auth');
 
 describe('Auth Middleware', function() {
   it('should throw an error if no authorization header is present', function() {
@@ -15,6 +18,29 @@ describe('Auth Middleware', function() {
     const req = {
       get: function(headerName) {
         return 'unittest';
+      }
+    };
+    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
+  });
+
+  it('should yield a userId after decoding the token', function() {
+    const req = {
+      get: function(headerName) {
+        return 'Bearer unittest';
+      }
+    };
+
+    sinon.stub(jwt, 'verify');
+    jwt.verify.returns({ userId: '123' });
+    authMiddleware(req, {}, () => {});
+    expect(req).to.have.property('userId');
+    jwt.verify.restore();
+  });
+
+  it('should throw an error if the token cannot be verified', function() {
+    const req = {
+      get: function(headerName) {
+        return 'Bearer unittest';
       }
     };
     expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
