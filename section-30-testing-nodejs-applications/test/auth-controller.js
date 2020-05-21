948cc30b01ba141052f83c1b43de454a7031ec8e
arleyribeiro <arley.sribeiro@gmail.com>
2020-05-21 07:00:43 -0300

    473. testing code with an active database

--- section-30-testing-nodejs-applications/test/auth-controller.js
+++ section-30-testing-nodejs-applications/test/auth-controller.js
@@ -1,10 +1,12 @@
 const expect = require('chai').expect;
-const jwt = require('jsonwebtoken');
 const sinon = require('sinon');
+const mongoose = require('mongoose');
 
 const User = require('../models/user');
 const AuthController = require('../controllers/auth');
 const StatusCode = require('../constants/statusCode');
+
+const MONGODB_URI_TEST = "mongodb+srv://arley:9IaUYwLsVYJVj5RV@cluster0-mhqji.mongodb.net/testdb?retryWrites=true&w=majority";
 
 describe('Auth Controller - Login', function() {
   it('should throw an error if accessing the database fail', function (done) {
@@ -24,5 +26,43 @@ describe('Auth Controller - Login', function() {
     })
 
     User.findOne.restore();
-  })
+  });
+
+  it('should send a response with a valid user status for an existing user', function (done) {
+    mongoose
+    .connect(MONGODB_URI_TEST, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        const user = new User({
+          email: 'email@email.com',
+          name: 'test',
+          password: 'asdfasdf',
+          posts: [],
+          _id: '5c0f66b979af55031b34728a'
+        });
+        return user.save();
+    })
+    .then(user => {
+      const req = {
+        userId: '5c0f66b979af55031b34728a'
+      }
+      const res = {
+        userId: user._id,
+        statusCode: StatusCode.INTERNAL_SERVER_ERROR,
+        userStatus: null,
+        status: function(code) {
+          this.statusCode = code;
+          return this;
+        },
+        json: function(data) {
+          this.userStatus = data.status
+        }
+      };
+      AuthController.getUserStatus(req, res, () => {}).then(() => {
+        expect(res.statusCode).to.be.equal(StatusCode.OK);
+        expect(res.userStatus).to.be.equal("NEW_USER");
+        done();
+      })
+    })
+    .catch(err => console.log(err));
+  });
 });
